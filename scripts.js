a0900a00acf77f8f1d226fe6f1932df2373bd673
kavyarao02 <kavyaraobaroda@gmail.com>
2024-11-27 14:10:21 +0530

    added scripts.js

---
+++ scripts.js
@@ -0,0 +1,32 @@
+// navigation scroll krne ke liye
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// nav bar m current sec select hota h
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav a');
+
+window.onscroll = () => {
+    var current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - 60) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        if (li.getAttribute('href').includes(current)) {
+            li.classList.add('active');
+        }
+    });
+};
